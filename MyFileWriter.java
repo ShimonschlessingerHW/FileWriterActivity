fe4556b2f0e47ec1fef6cd8b7c525e0feaae2edb
Shimon Schlessinger <sschlessinger1@hwemail.com>
2025-09-10 21:07:01 -0700

    Added stringify method (#1)
    Co-authored-by: Shimon Schlessinger <sschlessinger@hwemail.com>

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,6 +1,22 @@
 import java.io.*;
 
 public class MyFileWriter {
+    public static String stringify(String filePath) {
+        File file = new File(filePath);
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (Exception e){
+            // throw new IOException();
+        }
+
+        return sb.toString();
+    }
+
     private static void printFileSize(String fileName) {
         File file = new File("./" + fileName);
         long length = file.length();
@@ -29,5 +45,6 @@ public class MyFileWriter {
         generateHiddenFile("file.txt", "great assignment! love this!! so much fun!!!");
         generateHiddenFile("Folder", "file.txt", "this is very different");
         printFileSize(".file.txt");
+        System.out.println(stringify(".file.txt"));
     }
 }
